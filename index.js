32518a9249ccfc6ec0111904054438ca01d3c8eb
Mauro Doganieri <mauro.doganieri@gmail.com>
2019-07-24 01:52:05 +0200

    Refactored unzip/download process. Created siblings directories through Promise.all

--- index.js
+++ index.js
@@ -8,6 +8,77 @@ const directory = require('./utils/directory')
 const download = require('./utils/download')
 const unzip = require('./utils/unzip')
 
+// Directory
+const errOnMakeDir = directory.errOnMakeDir
+const makeBuildDir = directory.makeMkDir(cfg.directories.build.path)
+const makeSourcesDir = directory.makeMkDir(cfg.directories.sources.path)
+const makeHeadersDir = directory.makeMkDir(cfg.directories.headers.path)
+/* function makeGetOptObjs(cfg) {
+    return function getOptObjs(dir) {
+        return {
+            path: cfg.directories[dir].path,
+            options: undefined
+        }
+    }
+
+}
+const optObjs = Object.keys(cfg.directories).map(makeGetOptObjs(cfg)) */
+const optObjs = [
+    {
+        path: cfg.directories.headers.path
+    },
+    {
+        path: cfg.directories.sources.path
+    }
+]
+const makeSiblingDirs = directory.makeMkDirs(optObjs)
+
+// Download
+const sourcesUrl = cfg.nodeRelease.url + '/' + cfg.nodeRelease.files.headers
+const sourcesDestFile = path.join(cfg.directories.sources.path, cfg.nodeVersion + '.tar.gz')
+const downloadSources = download.makeDownloadSources(sourcesUrl, sourcesDestFile)
+
+function downloadSuccess() {
+    return true
+}
+
+function downloadSourcesAndNotify() {
+    return downloadSources()
+        .then(downloadSuccess)
+}
+
+// Unzip
+const unzipSources = unzip.makeUnzip(sourcesDestFile, cfg.directories.sources.path)
+
+function unzipIfDownloadSuccess(success) {
+    if (success) {
+        console.log('download happened')
+        return unzipSources()
+    } else {
+        console.log('download not happened')
+    }
+}
+
+// Copy
+function makeGetPathObjects(cfg) {
+    return function getPathObjects(file) {
+        return {
+            src: path.join(cfg.directories.sources.path, cfg.headerFiles.path, file.name),
+            dest: path.join(cfg.directories.headers.path, file.name)
+        }
+    }
+}
+const paths = cfg.headerFiles.files.map(makeGetPathObjects(cfg))
+const copyHeaders = copy.makeCopyFiles(paths)
+
+// Unzip and/or Download
+function unzipOrDownloadSources() {
+    return unzipSources()
+        .catch(downloadSourcesAndNotify)
+        .then(unzipIfDownloadSuccess)
+}
+
+// End build handlers
 function onEndTaskSuccess(res) {
     console.log('onEndTaskSuccess')
 }
@@ -23,59 +94,15 @@ function onEndTaskFail(err) {
     console.error('onEndTaskFail', err)
 }
 
-// Directory
-const errOnMakeDir = directory.errOnMakeDir
-const makeBuildDir = directory.makeMkDir(cfg.directories.build.path)
-const makeSourcesDir = directory.makeMkDir(cfg.directories.sources.path)
-const makeHeadersDir = directory.makeMkDir(cfg.directories.headers.path)
-
-// Sources
-const sourcesUrl = cfg.nodeRelease.url + '/' + cfg.nodeRelease.files.headers
-const sourcesDestFile = path.join(cfg.directories.sources.path, cfg.nodeVersion + '.tar.gz')
-const downloadSources = download.makeDownloadSources(sourcesUrl, sourcesDestFile)
-
-// Unzip
-const unzipSources = unzip.makeUnzip(sourcesDestFile, cfg.directories.sources.path)
-
-// Copy
-function makeGetPathObjects(cfg) {
-    return function getPathObjects(file) {
-        return {
-            src: path.join(cfg.directories.sources.path, cfg.headerFiles.path, file.name),
-            dest: path.join(cfg.directories.headers.path, file.name)
-        }
-    }
-}
-const paths = cfg.headerFiles.files.map(makeGetPathObjects(cfg))
-const copyHeaders = copy.makeCopyFiles(paths)
-
-// Unzip or Download
-function downloaded() {
-    return true
-}
-function downloadSourcesAndNotify() {
-    return downloadSources()
-        .then(downloaded)
-}
-function unzipOrDownloadSources() {
-    return unzipSources()
-        .catch(downloadSourcesAndNotify)
-        .then(downloadHappened => {
-            if (downloadHappened) {
-                console.log('download happened')
-                return unzipSources()
-            } else {
-                console.log('download not happened')
-            }
-        })
-}
-
+// Run pipeline
 makeBuildDir()
-    .catch(errOnMakeDir)
-    .then(makeSourcesDir)
-    .catch(errOnMakeDir)
-    .then(makeHeadersDir)
-    .catch(errOnMakeDir)
+    .then(makeSiblingDirs)
+    //.catch(errOnMakeDir)
+    //.then(makeSourcesDir)
+    //.catch(errOnMakeDir)
+    //.then(makeHeadersDir)
+    //.catch(errOnMakeDir)
+    // makeDirectories()
     .then(unzipOrDownloadSources)
     //.then(downloadSources)
     //.then(unzipSources)
